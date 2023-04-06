fe47f1c33a5fc37ed6cd7c238c2115db8058706a
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-06 10:16:06 -0700

    added conditional logic to window.prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,24 @@
 // Assignment code here
+// var Question1 = "choose between 8-12 password characters";
+/*function generatePassword(length, numbers, uppercase, lowercase, symbols)
+
+if(numbers){
+  characters += '0123456789'
+}
+*/
+var passwordLength =  126
+var characterTypes = ["0" "1" "2" "3" "4" "5" "6" "7" "8" "9"];
+var specialCharacters = ["!" "@" "#" "$" "%" "&" "*"];
+//
+
+window.prompt("Select password length between 8 and 126 characters");
+if 
+
+window.prompt("select character types: numbers and/or letters");
+window.prompt("Would you like to include uppercase, lowercasae, numeric, and symbols?") 
 
 
-// Get references to the #generate element
+/* Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); // this line creates a variable with a value fetched from html page using queryselector + the id
 var Char = ["0", "1", "2", "3", "4", "5"]; // 6789abcdefghijklmnopqrstuvwxyz!@*()_=+ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var passwordLength = 128;
@@ -33,8 +50,9 @@ if(generateBtn.prompt) {
  
  
  // this is only happening when I refresh the page. I might need to override default behavior of AP
-/*function generateBtn(event) {
+function generateBtn(event) {
 let generateBtn = event;
 generateBtn.preventDefault();
 
 }
+*/
