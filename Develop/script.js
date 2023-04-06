d2145ec18457ec7b20813281a7d336b723ca7ea2
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-06 16:48:11 -0700

    added for loop

--- Develop/script.js
+++ Develop/script.js
@@ -2,62 +2,63 @@
 // var Question1 = "choose between 8-12 password characters";
 /*function generatePassword(length, numbers, uppercase, lowercase, symbols)
 
-if(numbers){
-  characters += '0123456789'
-}
+
 */
-var passwordLength =  126
+var PasswordArray = "";
+var passwordLength = 126; 
 var numbers= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "*"];
+//
+var Length = confirm("Select password length between 8 and 126 characters");
+var lettersNumbers = confirm("Include numbers and letters?");
+var Symbols = confirm("Include uppercase, lowercasae, numeric, and symbols?");
+    
+function getInputValues() {
 
-window.prompt("Select password length between 8 and 126 characters");
-window.prompt("include numbers and letters?");
-  let inputs = '';
-    function getInputValues() {
-      if(numbers) {
-        return numbers
+      if (passwordLength >= 8 && passwordLength <= 126) {
+        PasswordArray.concat(passwordLength)
+      }
+    }
+    console.log(passwordLength)
+      /*
+      else if(numbers) {
+        PasswordArray.concat(numbers)
       }
       else if(letters) {
-        return letters
+        PasswordArray.concat(letters) 
+        }
+      else if(upperCase) {
+        PasswordArray.concat(upperCase)
       }
-      else
-        return letters + numbers
+      else {
+        PasswordArray
       }
+    }
   
-window.prompt("Yes or no, would you like to include uppercase, lowercasae, numeric, and symbols?") 
-  If 
+    
+ //for loop function all data, then script math.random & math.floor to return randomized character array
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      PasswordArray.push(element)
 
-/* Get references to the #generate element
+      return PasswordArray
+    }
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); // this line creates a variable with a value fetched from html page using queryselector + the id
-var Char = ["0", "1", "2", "3", "4", "5"]; // 6789abcdefghijklmnopqrstuvwxyz!@*()_=+ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var passwordLength = 128;
-var password = "";
-for (var i=0; i <= passwordLength; i++) {
-console.log(Char[i]);
-}
-
-// return password;
+getInputValues()
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-
-var PasswordCriteria = prompt("passwords must contain at least 8 characters");
-
 generateBtn.addEventListener("click", writePassword) 
-
-if(generateBtn.prompt) {
-  alert("Password must contain at least 8 characters & two special characters") }
- 
  
  // this is only happening when I refresh the page. I might need to override default behavior of AP
 function generateBtn(event) {
@@ -65,4 +66,3 @@ let generateBtn = event;
 generateBtn.preventDefault();
 
 }
-*/
