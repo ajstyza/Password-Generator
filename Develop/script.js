fa6ede0810e72dd9453949b529ec26a89d8dc914
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-07 00:15:36 -0700

    altered logic and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,75 +1,74 @@
-// Assignment code here
-// var Question1 = "choose between 8-12 password characters";
-/*function generatePassword(length, numbers, uppercase, lowercase, symbols)
 
-
-*/
-var PasswordArray = "";
-var passwordLength = 126; 
+var PasswordArray = [];
 var numbers= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "*"];
-// I want to call these variables for each prompt, so use var hasSpecial characters = confirm ('click ok')
-var Length = confirm("Password length must be between 8 and 126 characters");
-var lettersNumbers = confirm("Include numbers and letters?");
-var Symbols = confirm("Include uppercase, lowercasae, numeric, and symbols?");
+//
     
-function getInputValues() {
+function generatePassword() {
+  var password = ""
+  // ask user length of password
+  var passwordLength = prompt("What's the length of password you");
+  passwordLength= parseInt(passwordLength);
 
-      if (passwordLength >= 8 && passwordLength <= 126) {
-        PasswordArray.concat(passwordLength)
-      }
-      else if(numbers) {
-        PasswordArray.concat(numbers + passwordLength)
-      }
-      else if(letters) {
-        PasswordArray.concat(letters + numbers) 
-        }
-      else if(upperCase) {
-        PasswordArray.concat(upperCase + letters + numbers)
-      }
-      else {
-        PasswordArray
-      }
+  // check length -  if not valid stop
+  if (passwordLength < 8 || passwordLength > 126) {
+    // stop 
+    return;
+  }
 
-      for (let i = 0; i < PasswordArray.length; i++) {
-        const element = PasswordArray[i];
-        // math.random 
-    }
-    return PasswordArray.join("");
-  
+  // valid length
+  var wantUppercase = confirm("Include uppercase?");
+  // YES / NO
+  if (wantUppercase) {
+    PasswordArray = PasswordArray.concat(upperCase)
   }
   
+  var wantLowercase = confirm("Include lowercasae?");
+  if (wantLowercase) {
+    PasswordArray = PasswordArray.concat(letters)
+  }
+  var numbers = confirm("include numbers?")
+  if (numbers) {
+    PasswordArray = PasswordArray.concat(numbers)
+  }
+  var specialCharacters = confirm("Include special characters?")
+  if (specialCharacters) {
+    PasswordArray = PasswordArray.concat(specialCharacters)
+  }
+  
+      for (let i = 0; i < passwordLength; i++) {
 
-    /*
-    
- //for loop function all data, then script math.random & math.floor to return randomized character array
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      PasswordArray.push(element)
-
-      return PasswordArray
+        var rnum = Math.floor(Math.random() * PasswordArray.length);
+        var rchar = PasswordArray[rnum];
+        password = password+rchar;
+        
     }
+    return password
+  
+  }
 
 
 // Get references to the #generate element
 
 var generateBtn = document.querySelector("#generate"); // this line creates a variable with a value fetched from html page using queryselector + the id
-getInputValues()
+//writePassword()
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
+  event.preventDefault();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
-/*
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword) 
  
- // this is only happening when I refresh the page. I might need to override default behavior of AP
-function generateBtn(event) {
-let generateBtn = event;
-generateBtn.preventDefault(); */
+ // this is only happening when I refresh the page. I might need to override default behavior of API
+// function generateBtn(event) {
+// let generateBtn = event;
+// generateBtn.preventDefault(); 
+// }
