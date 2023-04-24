ac3ebbd5a4474a0e530ca94496d5da549b42cee4
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-04-23 17:45:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
-var PasswordArray = [];
-var numbers= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacters = ["!", "@", "#", "$", "%", "&", "*"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharactersArr = ["!", "@", "#", "$", "%", "&", "*"];
 //
-    
+
 function generatePassword() {
+  var PasswordArray = [];
   var password = ""
-  // ask user length of password
   var passwordLength = prompt("How long would you like your password, pick between 8-126 characters");
-  passwordLength= parseInt(passwordLength);
+
+  passwordLength = parseInt(passwordLength);
   if (passwordLength < 8 || passwordLength > 126) {
-    return;
+    return "Invalid Length";
   }
   var wantUppercase = confirm("Include uppercase?");
   if (wantUppercase) {
@@ -24,23 +24,27 @@ function generatePassword() {
   }
   var numbers = confirm("include numbers?")
   if (numbers) {
-    PasswordArray = PasswordArray.concat(numbers)
+    PasswordArray = PasswordArray.concat(numbersArr)
   }
   var specialCharacters = confirm("Include special characters?")
   if (specialCharacters) {
-    PasswordArray = PasswordArray.concat(specialCharacters)
+    PasswordArray = PasswordArray.concat(specialCharactersArr)
+  }
+  //console.log("PA",PasswordArray)
+  if(PasswordArray.length === 0){
+    return "Choose 1"
   }
 
-      for (let i = 0; i < passwordLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
 
-        var rnum = Math.floor(Math.random() * PasswordArray.length);
-        var rchar = PasswordArray[rnum];
-        password = password+rchar;
-        
-    }
-    return password
-  
+    var rnum = Math.floor(Math.random() * PasswordArray.length);
+    var rchar = PasswordArray[rnum];
+    password = password + rchar;
+
   }
+  return password
+
+}
 
 
 // Get references to the #generate element
@@ -57,5 +61,4 @@ function writePassword(event) {
 
 }
 
-generateBtn.addEventListener("click", writePassword) 
- 
+generateBtn.addEventListener("click", writePassword)
